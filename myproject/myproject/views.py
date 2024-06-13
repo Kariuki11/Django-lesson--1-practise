4aa5330f056d5640f1e462a2657203ad390fd2f8
Kariuki11 <irungukenneth433@gmail.com>
2024-06-13 03:11:17 +0300

    New commits

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Hello World! I'm home")
+    # return HttpResponse("Hello World! I'm home")
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse("This is the about page")
+    # return HttpResponse("This is the about page")
+    return render(request, 'about.html')
